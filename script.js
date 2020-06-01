8a8e90d821a42ece55f6665dc9a57688634a65f7
VictorMH77 <victor.mheringer@gmail.com>
2020-06-01 15:39:02 -0300

    VictorMH77

---
+++ script.js
@@ -0,0 +1,16 @@
+function sobre(){
+    var divTeste = document.getElementById("teste1");
+    divTeste.innerHTML = "O meu nome é Victor Macedo Heringer, tenho 18 anos e sou estudante do curso de Sistemas de Informação da UVV. Apesar de gostar muito de tecnologia em geral, não sou muito familiarizado com a área da programação. Escolhi esse curso pelo fato de que sempre tive curiosidade de entrar nessa área e também por acreditar que o futuro está presente nessa área. Almejo um dia conseguir realizar os desejos que tenho sobre ideia de projetos que tenho relacionado a essa área. "
+}
+
+function teste(){
+    var divTeste = document.getElementById("teste1");
+    divTeste.innerHTML = 
+    "<strong>INSTAGRAM:</strong> @victor_heringer <br> <strong>TWITTER:</strong> @victormh0 <br> <strong>GITHUB:</strong> VictorMH77"
+}
+
+function contato(){
+    var divTeste = document.getElementById("teste1");
+    divTeste.innerHTML = 
+    "<strong>EMAIL:</strong> victor.mheringer@gmail.com <br> <strong>TELEFONE:</strong> (27) 99297-8705"
+}
